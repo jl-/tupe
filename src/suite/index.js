7e558aaf8cc51ac82c05c3b916d27d8bad7e5a3e
z.cius <zr.cius@gmail.com>
2018-10-12 14:05:25 +0800

    parse path info and gen html content

--- src/suite/index.js
+++ src/suite/index.js
@@ -5,22 +5,31 @@ import * as status from '../meta/status';
 
 export default class Suite {
     constructor (fpath, tmpdir) {
+        this.fpath = fpath;
         this.status = status.INIT;
 
-        this.htmlName = `tmp-${hash(fpath)}.html`;
-        this.htmlPath = path.resolve(tmpdir, this.htmlName);
-        this.path = path.relative(path.dirname(this.htmlPath), path.resolve(fpath));
+        tmpdir = path.join(tmpdir, path.basename(tmpdir));
+        this.name = `tmp-${hash(fpath)}.html`;
+        this.path = path.resolve(tmpdir, this.name);
 
-        fs.outputFileSync(this.htmlPath, this.html);
+        fs.outputFileSync(this.path, genHtml(this.path, fpath));
     }
 
-    get html () {
-        return `
-            <html>
-                <body>
-                    <script src="${this.path}"></script>
-                </body>
-            </html>
-        `;
+    forServer (port, host = 'localhost') {
+        this.url = `http://${host}:${port}/${this.name}`;
     }
+}
+
+function genHtml (destPath, entryPath) {
+    const dir = path.dirname(destPath);
+    const relpath = path.relative(dir, entryPath);
+
+    return `
+        <html>
+            <body>
+                <script src="${relpath}"></script>
+                <script>tupe.run();</script>
+            </body>
+        </html>
+    `;
 }

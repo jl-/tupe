95c555d0402458716a143c9b14a7549b1d4b720d
z.cius <zr.cius@gmail.com>
2018-10-13 18:21:26 +0800

    print debug info

--- src/reporter/index.js
+++ src/reporter/index.js
@@ -1,5 +1,23 @@
+import path from 'path';
+import figures from 'figures';
+import string from '../utils/string';
+import logger from '../utils/logger';
+
 export default class Reporter {
     receive (suite, spec) {
-        console.log(spec);
+        const p = suite.srcpath.split(path.sep).join(
+            string.gray(` ${figures.pointerSmall} `)
+        );
+        const message = [p, spec.title].join(
+            string.gray(` ${figures.arrowRight} `)
+        );
+
+        logger[spec.passed ? 'success' : 'error'](message);
+    }
+
+    sumup (suites, metric) {
+        console.log(suites);
+        console.log(metric);
+        logger.finish('bb');
     }
 }

a2acbc3f5da88e6d6d557e9d2df6578a9ff26156
z.cius <zr.cius@gmail.com>
2018-10-14 21:54:28 +0800

    remove unused code

--- src/utils/fs.js
+++ src/utils/fs.js
@@ -1,15 +1,6 @@
-import glob from 'glob';
 import fs from 'fs-extra';
 import { intercept } from './proxy';
 
-const extensions = {
-    async resolveGlob (pattern, options) {
-        return new Promise((resolve, reject) => {
-            glob(pattern, options, (err, paths) => {
-                return err ? reject(err) : resolve(paths);
-            });
-        });
-    }
-};
+const extensions = {};
 
 export default intercept(fs, extensions);

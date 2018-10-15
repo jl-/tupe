dc12342a66d1e01a80c4ba5eeaaf43da1b7e4652
z.cius <zr.cius@gmail.com>
2018-10-15 22:39:18 +0800

    add isFailed isPassed util

--- src/meta/status.js
+++ src/meta/status.js
@@ -2,3 +2,6 @@ export const INIT = 0;
 export const PENDING = 1;
 export const FAILED = 2;
 export const PASSED = 3;
+
+export const isFailed = status => status === FAILED;
+export const isPassed = status => status === PASSED;
